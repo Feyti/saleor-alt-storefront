ff31354499a0797dfabe25e79dd6bee1376e0739
Gobz <raniahgobz@outlook.ocm>
2022-03-27 17:56:51 +0000

    corrected bug in apollo config

--- apollo.config.js
+++ apollo.config.js
@@ -3,6 +3,7 @@ module.exports = {
     service: {
       name: "saleor",
       url: "https://mhbvufznnx.us-east-1.awsapprunner.com/graphql/",
+      headers: "", // local authority
       skipSSLValidation: true,
     },
   },
