629a2c9b780474f75e0e157737776ad05df53f69
Gobz <raniahgobz@outlook.ocm>
2022-04-03 10:47:56 +0000

    updated the token

--- apollo.config.js
+++ apollo.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: "saleor",
       url: "https://mhbvufznnx.us-east-1.awsapprunner.com/graphql/",
       headers: {
-        Authorization: "Ql4I7RSxi6SnBJqliQ5DIWciE2W6N0",
+        Authorization: "bearer Ql4I7RSxi6SnBJqliQ5DIWciE2W6N0",
       }, // local authority
       // TODO: add the headers for some real secure queries
       skipSSLValidation: true,
