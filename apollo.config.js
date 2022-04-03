7bcc274ad407e8af2742eb91e27d408fe3e24625
michaelgobz <raniahgobz@outlook.com>
2022-04-03 13:38:31 +0300

    added the api auth token

--- apollo.config.js
+++ apollo.config.js
@@ -3,7 +3,9 @@ module.exports = {
     service: {
       name: "saleor",
       url: "https://mhbvufznnx.us-east-1.awsapprunner.com/graphql/",
-      headers: "", // local authority
+      headers: {
+        Authorization: "Ql4I7RSxi6SnBJqliQ5DIWciE2W6N0",
+      }, // local authority
       // TODO: add the headers for some real secure queries
       skipSSLValidation: true,
     },
